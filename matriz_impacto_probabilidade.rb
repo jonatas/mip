6f3a6733967c882fa9afffde520b1be5bc5d87ce
Jonatas <jonatasdp@gmail.com>
2009-09-09 08:00:31 -0300

    adicionado o exercicio da aula do dia 8 adicionado ordem de prioridade baseada no indice de risco

--- matriz_impacto_probabilidade.rb
+++ matriz_impacto_probabilidade.rb
@@ -69,6 +69,7 @@ dimensiona_risco = lambda do |matriz_risco|
 end
 
 # mesmo exemplo que http://twitpic.com/g5r2z
+=begin
 matriz_risco = {
   :probabilidade => {
     :alt => ["C", "P", "Q", "T", "$", "L"]
@@ -78,16 +79,86 @@ matriz_risco = {
     :alt  => ["T", "$", "L"]
   }
 }
+=end
 
-risco = 0.0 
+requisitos = [
+{
+  :probabilidade => {
+    :med => %w(P C),
+    :alt => %w(Q L),
+    :malt => %w($ T)
+  },
+  :impacto => {
+    :malt => %w(P C $ T),
+    :alt => %w(Q L)
+  }
+},{
+  :probabilidade => {
+    :mbx => %w(C P),
+    :alt => %w($ T),
+    :malt => %w(Q L)
+  },
+  :impacto => {
+    :malt => %w(Q L),
+    :alt => %w($ T),
+    :med => %w(C P)
+  }
+},{
+  :probabilidade => {
+    :bx => %w(Q L),
+    :alt => %w($ T),
+    :malt => %w(P C)
+  },
+  :impacto => {
+    :malt => %w(P C),
+    :med => %w($ T),
+    :bx => %w(Q L)
+  }
+},{
+  :probabilidade => {
+    :bx => %w(P C),
+    :alt => %w($),
+    :malt => %w(Q T)
+  },
+  :impacto => {
+    :malt => %w($ Q),
+    :med => %w(T),
+    :bx => %w(P C)
+  }
+},{
+  :probabilidade => {
+    :alt => %w($),
+    :malt => %w(Q T P C)
+  },
+  :impacto => {
+    :malt => %w(T Q),
+    :alt => %w($),
+    :med => %w(P C)
+  }
+}
+]
 
-puts "dimensa|  impacto   | probabilid. | risco dimensao"
+ordem_de_prioridade = {}
+requisitos.each_with_index do |matriz_risco, i|
+  risco = 0.0 
 
-dimensiona_risco.call(matriz_risco).each do |dimensao, calculo|
-  risco_dimensao = calculo[:probabilidade].to_f*calculo[:impacto].to_f
-  risco += risco_dimensao
-  puts [dimensao, calculo[:impacto], calculo[:probabilidade], risco_dimensao].join("     |     ")
+  puts "requisito: #{i}"
+  puts "dimensa|  impacto   | probabilid. | risco dimensao"
+
+  dimensiona_risco.call(matriz_risco).each do |dimensao, calculo|
+    risco_dimensao = calculo[:probabilidade].to_f*calculo[:impacto].to_f
+    risco += risco_dimensao
+    puts [dimensao, calculo[:impacto], calculo[:probabilidade], risco_dimensao].join("     |     ")
+  end
+
+  ordem_de_prioridade[i] = risco
+  puts risco
+  puts "--------------------"
 end
 
-puts risco
+puts "ordem de prioridade"
+puts "requisito | indice risco"
+ordem_de_prioridade.sort_by{|ordem, indice_risco|indice_risco}.reverse.each do |requisito,indice_risco|
+  puts "      #{requisito}   |   #{indice_risco}"
+end
 
